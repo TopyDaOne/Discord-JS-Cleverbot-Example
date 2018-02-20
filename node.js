11ca831963a797e960eae677a9f10e8368b6ff13
Hey <18427051+Hey@users.noreply.github.com>
2018-02-20 15:44:16 +0100

    The code is added.
    I've added the example code.

---
+++ node.js
@@ -0,0 +1,24 @@
+// Getting discord.js to work.
+const Discord = require('discord.js')
+const client = new Discord.Client();
+
+// Cleverbot setup:
+const Cleverbot = require("cleverbot-node");
+const clbot = new Cleverbot;
+clbot.configure({botapi: "YOUR API KEY HERE, CHECK README.MD"});
+
+// This will make the bot reply when it gets mentioned.
+client.on("message", (message) => {
+  if (message.content.startsWith("<@REPLACE WITH YOUR BOT ID>")) {
+    clbot.write(message.content, (response) => {
+      message.channel.startTyping();
+      setTimeout(() => {
+        message.channel.send(response.output).catch(console.error);
+        message.channel.stopTyping();
+      }, Math.random() * (1 - 3) + 1 * 1000);
+    });
+  }
+})
+
+// Make the application able to login to your bot.
+client.login('YOUR BOT TOKEN');
